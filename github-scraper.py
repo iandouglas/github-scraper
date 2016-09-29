8e820a5252af45d0f84aa6ae7eafdc248d6b77e9
ian douglas <ian.douglas@iandouglas.com>
2016-09-29 16:48:44 -0600

    first deploy

---
+++ github-scraper.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/<string:site>/<string:account>/<string:repo>')
+def scraper_nometadata(site, account, repo):
+    return scraper(site, account, repo, None)
+
+
+@app.route('/<string:site>/<string:account>/<string:repo>/<string:metadata>')
+def scraper(site, account, repo, metadata):
+    url = 'https://{site}/{account}/{repo}'.format(
+        site=site,
+        account=account,
+        repo=repo,
+    )
+    counters = []
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    counters = soup.select(".counter")
+    counter_position = None
+
+    if metadata:
+        if metadata == 'issues':
+            counter_position = 0
+        elif metadata == 'pull_requests':
+            counter_position = 1
+        return jsonify(counters[counter_position].contents[0])
+
+
+    return jsonify({
+        'issues': counters[0].contents[0],
+        'pull_requests': counters[1].contents[0],
+    })
+
+if __name__ == '__main__':
+    app.run()
